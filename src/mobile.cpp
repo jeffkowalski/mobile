467ea225b5a6a29f6233eff7951c120e7e89be04
Jeff Kowalski <jeff.kowalski@gmail.com>
2021-09-15 22:52:32 -0700

    add readme; use descriptive constants and format source

--- src/mobile.cpp
+++ src/mobile.cpp
@@ -1,19 +1,24 @@
 #include <Arduino.h>
 
+int const directionPin = 12;
+int const pwmPin       = 3;
+int const brakePin     = 9;
+
 void setup() {
-  //Setup Channel A
-  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
-  pinMode(9, OUTPUT);  //Initiates Brake Channel A pin
-  delay(1000);
+    // Setup Channel A
+    pinMode (directionPin, OUTPUT);  // Initiates Motor Channel A pin
+    pinMode (brakePin, OUTPUT);      // Initiates Brake Channel A pin
+    delay (1000);
 }
 
-void loop(){
-  digitalWrite(12, LOW); //Establishes forward direction of Channel A
-  digitalWrite(9, LOW);  //Disengage the Brake for Channel A
+void loop() {
+    digitalWrite (directionPin, LOW);  // Establishes forward direction of Channel A
+    digitalWrite (brakePin, LOW);      // Disengage the Brake for Channel A
 
-  analogWrite(3, 120);   //Spins the motor on Channel A at slow speed
-  delay(2000);
+    analogWrite (pwmPin, 120);  // Spins the motor on Channel A at slow speed
+    delay (2000);
 
-  digitalWrite(9, HIGH); //Engage the Brake for Channel A
-  delay (3000);
+    digitalWrite (brakePin, HIGH);  // Engage the Brake for Channel A
+    analogWrite (pwmPin, 0);        // Turn motor off
+    delay (3000);
 }
