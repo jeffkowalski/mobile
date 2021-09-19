57f875f4280725e94252125e3da3ba2f788c2449
Jeff Kowalski <jeff.kowalski@gmail.com>
2021-09-18 22:29:13 -0700

    don't brake; easier-to-edit pulses (facilitates experimentation)

--- src/mobile.cpp
+++ src/mobile.cpp
@@ -6,19 +6,32 @@ int const brakePin     = 9;
 
 void setup() {
     // Setup Channel A
-    pinMode (directionPin, OUTPUT);  // Initiates Motor Channel A pin
-    pinMode (brakePin, OUTPUT);      // Initiates Brake Channel A pin
-    delay (1000);
+    pinMode (directionPin, OUTPUT);    // Initiates Motor Channel A pin
+    pinMode (brakePin, OUTPUT);        // Initiates Brake Channel A pin
+    digitalWrite (directionPin, LOW);  // Establishes forward direction of Channel A
 }
 
+typedef struct {
+    unsigned char pwm;
+    int           duration;
+} Pulse;
+
+Pulse pulses[] = {
+    {100, 200},  //
+    {200, 500},  //
+    {255, 700},  //
+    {0, 15000},   //
+};
+
+int const num_pulses = sizeof (pulses) / sizeof (Pulse);
+
 void loop() {
-    digitalWrite (directionPin, LOW);  // Establishes forward direction of Channel A
-    digitalWrite (brakePin, LOW);      // Disengage the Brake for Channel A
+    digitalWrite (brakePin, LOW);  // Disengage the Brake for Channel A
 
-    analogWrite (pwmPin, 120);  // Spins the motor on Channel A at slow speed
-    delay (2000);
+    for (int ii = 0; ii < num_pulses; ++ii) {
+        analogWrite (pwmPin, pulses[ii].pwm);  // Spins the motor on Channel A at slow speed
+        delay (pulses[ii].duration);
+    }
 
-    digitalWrite (brakePin, HIGH);  // Engage the Brake for Channel A
-    analogWrite (pwmPin, 0);        // Turn motor off
-    delay (3000);
+    //    digitalWrite (brakePin, HIGH);  // Engage the Brake for Channel A
 }
